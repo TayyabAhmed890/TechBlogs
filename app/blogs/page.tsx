77e3c66f76e3ec8bcc9bb115f1615d1cd86756e4
Workplace <tayyabahmed7561@gmail.com>
2024-12-24 14:48:11 -0800

    done

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import {Montserrat} from 'next/font/google'
+import { blogData } from "./data/blogs";
 
 const Mont = Montserrat({
   subsets:["latin"],
@@ -7,46 +8,7 @@ const Mont = Montserrat({
 });
 
 
-export let blogData = [
-  {
-    name: "blog1",
-    title: "Introduction to PHP",
-    description: "Learn the basics of PHP, a powerful scripting language widely used for web development.",
-    image: "https://images.unsplash.com/photo-1599507593499-a3f7d7d97667?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "blog2",
-    title: "Meta: Shaping the Future of Social Media",
-    description: "Dive into Meta's vision for the future, redefining the way we connect and interact online.",
-    image: "https://images.unsplash.com/photo-1665799871677-f1fd17338b43?q=80&w=1414&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "blog3",
-    title: "What is Cloud Computing?",
-    description:
-      "Cloud computing is a revolutionary technology that allows individuals and businesses to access computing resources",
-    image: "https://images.unsplash.com/photo-1667984390527-850f63192709?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "blog4",
-    title: "The Importance of Programming",
-    description: "Discover why programming is a vital skill in the modern world, empowering innovation and problem-solving.",
-    image: "https://images.unsplash.com/photo-1607799279861-4dd421887fb3?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "blog5",
-    title: "The AI Era: Transforming Our World",
-    description: "Explore how artificial intelligence is revolutionizing industries and shaping the future.",
-    image: "https://plus.unsplash.com/premium_photo-1683121710572-7723bd2e235d?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "blog6",
-    title: "What is Blockchain?",
-    description:
-      "Blockchain is a decentralized, secure digital ledger technology that records transactions across a network.",
-    image: "https://images.unsplash.com/photo-1640161704729-cbe966a08476?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-];
+
 
 const Blogs = () => {
   return (
